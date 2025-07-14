0670cf55ed356089b14be33b7fe0685c1298c067
Justin Matthew Lee <justin.matthew.lee.18@gmail.com>
2025-07-14 15:48:04 -0400

    Refactor with text file

--- main.py
+++ main.py
@@ -1,39 +1,25 @@
-"""
-This program will create a hockey quiz game.
-"""
-
-# Imports -----------------------------
 import time
 
-# Variables ---------------------------
-questions = ('How many periods in a hockey game? ',
-             'How many players do you start the game with? ',
-             'What number is Connor McDavid? ',
-             'Who is the coach of the New Jersey Devils? ',
-             'Which NHL team has a mask as part of their logo? '
-             )
-options = (('A. 1', 'B. 2','C. 3','D. 4','E. 5'),
-           ('A. 2', 'B. 7','C. 3','D. 4','E. 6'),
-           ('A. 98', 'B. 99','C. 97','D. 79','E. 89'),
-           ('A. Sheldon Keefe', 'B. Craig Berube','C. Paul Maurice','D. Jim Montgomery','E. Kyle Dubas'),
-           ('A. New Jersey Devils', 'B. Anaheim Ducks','C. Toronto Maple Leafs','D. Montreal Canadians','E. Detroit Red Wings')
-           )
-answers = (('C', 2),
-           ('E', 4),
-           ('C', 2),
-           ('A', 0),
-           ('B', 1)
-        )
+def load_quiz_data(filename):
+    questions = []
+    options = []
+    answers = []
+    with open(filename, 'r') as f:
+        lines = [line.strip() for line in f if line.strip()]
+    for i in range(0, len(lines), 7):
+        questions.append(lines[i])
+        options.append(tuple(lines[i+1:i+6]))
+        answers.append(lines[i+6].upper())
+    return questions, options, answers
+
+# Load quiz data from file
+questions, options, answers = load_quiz_data('quiz_data.txt')
 guesses = []
-question_num = 0
 score = 0
 
-# Code --------------------------------
-# Questions/Options/Guesses
-for question in questions:
-    question_num = question_num + 1
+# Questions, options and guesses loop
+for question_num, question in enumerate(questions, 1):
     print(f'\n{question_num}. {question}')
-    
     for i in range(5):
         print(f'-----{options[question_num-1][i]}')
     while True:
@@ -42,11 +28,9 @@ for question in questions:
             break
         else:
             print('Invalid entry please type (A/B/C/D/E).')
-
-    
     guesses.append(guess)
 
-# Answers
+# Prints quiz results and evaluates correct answers
 print('\n-----------------------')
 time.sleep(0.5)
 print('Evaluating...')
@@ -56,15 +40,15 @@ for i in range(3,0,-1):
 print()
 time.sleep(1)
 
-question_num = 0
-for i in range(5):
-    question_num += 1
-    if guesses[i] == answers[i][0]:
-        print(f'✓ - Question {question_num} was correct! You chose ({options[question_num-1][answers[i][1]]}).')
+for i in range(len(questions)):
+    if guesses[i] == answers[i]:
+        correct_index = ord(answers[i]) - ord('A')
+        print(f'✓ - Question {i+1} was correct! You chose ({options[i][correct_index]}).')
         score+=1
     else:
-        print(f'X - Question {question_num} was incorrect! The answer was ({options[question_num-1][answers[i][1]]}).')
+        correct_index = ord(answers[i]) - ord('A')
+        print(f'✖ - Question {i+1} was incorrect! The answer was ({options[i][correct_index]}).')
 
+# Prints final score
 time.sleep(1)
-
-print(f'\nYour score is {score}/5. {(score/5*100)}%')
+print(f'\nYour score is {score}/{len(questions)}. {(score/len(questions)*100)}%')
